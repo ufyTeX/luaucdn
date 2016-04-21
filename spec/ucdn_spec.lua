1c0bac36c2457ad2f71d5f85908c0f5a51eabda8
Deepak Jois <deepak.jois@gmail.com>
2016-04-21 20:36:00 +0530

    Added enumerated constants for Bidi_Class values

--- spec/ucdn_spec.lua
+++ spec/ucdn_spec.lua
@@ -3,6 +3,12 @@ local ucdn = require("ucdn")
 describe("ucdn module", function()
 
   it("contains v8.0.0 of the Unicode Character Database", function()
-    assert.are_equal("8.0.0", ucdn.UNICODE_VERSION)
+    assert.are_equal("8.0.0", ucdn.get_unicode_version())
+  end)
+
+  it("returns the bidi class for a given codepoint", function()
+    assert.are_equal(ucdn.UCDN_BIDI_CLASS_AL, ucdn.get_bidi_class(0x0627)) -- U+0627 ARABIC LETTER ALEF
+    assert.are_equal(ucdn.UCDN_BIDI_CLASS_L, ucdn.get_bidi_class(0x0907))  -- U+0907 DEVANAGARI LETTER I
+    assert.are_equal(ucdn.UCDN_BIDI_CLASS_WS, ucdn.get_bidi_class(0x0020)) -- U+0020 SPACE
   end)
 end)
--- src/luaucdn/luaucdn.c
+++ src/luaucdn/luaucdn.c
@@ -1,10 +1,25 @@
 #include "ucdn.h"
 
+#include <stdint.h>
+
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
 
+int get_unicode_version(lua_State *L) {
+  lua_pushstring(L, ucdn_get_unicode_version());
+  return 1;
+}
+
+int get_bidi_class(lua_State *L) {
+  uint32_t c = lua_tounsigned(L, 1);
+  lua_pushinteger(L, ucdn_get_bidi_class(c));
+  return 1;
+}
+
 static const struct luaL_Reg lib_table [] = {
+  {"get_unicode_version", get_unicode_version},
+  {"get_bidi_class", get_bidi_class},
   {NULL, NULL}
 };
 
