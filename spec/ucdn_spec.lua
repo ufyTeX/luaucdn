410043133085eeebe18e80599f66ad2e477963a8
Deepak Jois <deepak.jois@gmail.com>
2016-04-22 20:51:42 +0530

    Wrap ucdn_paired_bracket and ucdn_paired_bracket_type. Fix #3

--- spec/ucdn_spec.lua
+++ spec/ucdn_spec.lua
@@ -11,4 +11,20 @@ describe("ucdn module", function()
     assert.are_equal(ucdn.UCDN_BIDI_CLASS_L, ucdn.get_bidi_class(0x0907))  -- U+0907 DEVANAGARI LETTER I
     assert.are_equal(ucdn.UCDN_BIDI_CLASS_WS, ucdn.get_bidi_class(0x0020)) -- U+0020 SPACE
   end)
+
+  it("returns the Bidi_Paired_Bracket_Type for a given codepoint", function()
+    assert.are_equal(ucdn.UCDN_BIDI_PAIRED_BRACKET_TYPE_OPEN, ucdn.paired_bracket_type(0x0028))  -- U+0028 LEFT PARENTHESIS
+    assert.are_equal(ucdn.UCDN_BIDI_PAIRED_BRACKET_TYPE_CLOSE, ucdn.paired_bracket_type(0x0029)) -- U+0029 RIGHT PARENTHESIS
+    assert.are_equal(ucdn.UCDN_BIDI_PAIRED_BRACKET_TYPE_NONE, ucdn.paired_bracket_type(0x0041))  -- U+0041 LATIN CAPITAL LETTER A
+  end)
+
+  it("returns the corresponding Bidi_Paired_Bracket codepoint for a given codepoint", function()
+    -- LEFT PARENTHESIS and RIGHT PARENTHESIS
+    assert.are_equal(0x0029, ucdn.paired_bracket(0x0028))
+    assert.are_equal(0x0028, ucdn.paired_bracket(0x0029))
+
+    -- HALFWIDTH LEFT CORNER BRACKET and HALFWIDTH RIGHT CORNER BRACKET
+    assert.are_equal(0xFF62, ucdn.paired_bracket(0xFF63))
+    assert.are_equal(0xFF63, ucdn.paired_bracket(0xFF62))
+  end)
 end)
--- src/luaucdn/luaucdn.c
+++ src/luaucdn/luaucdn.c
@@ -17,9 +17,23 @@ int get_bidi_class(lua_State *L) {
   return 1;
 }
 
+int paired_bracket_type(lua_State *L) {
+  uint32_t c = lua_tounsigned(L, 1);
+  lua_pushinteger(L, ucdn_paired_bracket_type(c));
+  return 1;
+}
+
+int paired_bracket(lua_State *L) {
+  uint32_t c = lua_tounsigned(L, 1);
+  lua_pushinteger(L, ucdn_paired_bracket(c));
+  return 1;
+}
+
 static const struct luaL_Reg lib_table [] = {
   {"get_unicode_version", get_unicode_version},
   {"get_bidi_class", get_bidi_class},
+  {"paired_bracket_type", paired_bracket_type},
+  {"paired_bracket", paired_bracket},
   {NULL, NULL}
 };
 
