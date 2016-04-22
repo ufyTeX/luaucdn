6f1286e3223b8b80fa09f7b91e19e66e950672ca
Deepak Jois <deepak.jois@gmail.com>
2016-04-22 20:45:16 +0530

    Doc edits

--- spec/ucdn_spec.lua
+++ spec/ucdn_spec.lua
@@ -6,7 +6,7 @@ describe("ucdn module", function()
     assert.are_equal("8.0.0", ucdn.get_unicode_version())
   end)
 
-  it("returns the bidi class for a given codepoint", function()
+  it("returns the Bidi_Class propety value for a given codepoint", function()
     assert.are_equal(ucdn.UCDN_BIDI_CLASS_AL, ucdn.get_bidi_class(0x0627)) -- U+0627 ARABIC LETTER ALEF
     assert.are_equal(ucdn.UCDN_BIDI_CLASS_L, ucdn.get_bidi_class(0x0907))  -- U+0907 DEVANAGARI LETTER I
     assert.are_equal(ucdn.UCDN_BIDI_CLASS_WS, ucdn.get_bidi_class(0x0020)) -- U+0020 SPACE
